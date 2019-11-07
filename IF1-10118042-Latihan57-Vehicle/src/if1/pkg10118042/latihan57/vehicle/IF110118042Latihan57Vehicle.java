103191d7b5164dcfaa98321c0611eb178b9c9fd8
DhimasAjiG <55832635+DhimasAjiG@users.noreply.github.com>
2019-11-07 20:01:21 +0700

    Add files via upload

---
+++ IF1-10118042-Latihan57-Vehicle/src/if1/pkg10118042/latihan57/vehicle/IF110118042Latihan57Vehicle.java
@@ -0,0 +1,17 @@
+
+package if1.pkg10118042.latihan57.vehicle;
+/**
+ * @author
+ * Nama  : Dhimas Aji Ghairrahmat
+ * Kelas : IF1
+ * NIM   : 10118042
+ * Deskripsi Program : Program ini berfungsi untuk Menampilkan Vehicle
+ */
+public class IF110118042Latihan57Vehicle {
+
+    public static void main(String[] args) {
+        Bicycle bicycle = new Bicycle();
+        Skateboard skateboard = new Skateboard();
+    }
+    
+}
---
+++ IF1-10118042-Latihan57-Vehicle/src/if1/pkg10118042/latihan57/vehicle/Skateboard.java
@@ -0,0 +1,28 @@
+
+package if1.pkg10118042.latihan57.vehicle;
+
+
+public class Skateboard extends Vehicle{
+    private double myBoardLength;
+
+    public Skateboard() {
+        this.myBoardLength = 0;
+        setMyBrand("Ally Skate");
+        setMyModel("Rocket");
+        setMyBoardLength(54.5);
+
+        System.out.println("\nSkateboard");
+        System.out.println("Brand  : "+getMyBrand());
+        System.out.println("Model  : "+getMyModel());
+        System.out.println("Panjangnya Board: "+getMyBoardLength());
+    }
+
+    public double getMyBoardLength() {
+        return myBoardLength;
+    }
+
+    public void setMyBoardLength(double myBoardLength) {
+        this.myBoardLength = myBoardLength;
+    }
+    
+}
